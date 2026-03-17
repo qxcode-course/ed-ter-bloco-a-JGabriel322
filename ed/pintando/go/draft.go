65d29e851b41c730745cce9abb3749f26c52dfbf
JGabriel322 <joaogab322@gmail.com>
2026-03-17 14:00:53 +0000

    feito

---
+++ ed/pintando/go/draft.go
@@ -0,0 +1,16 @@
+package main
+import (
+    "fmt"
+    "math"
+)
+func main() {
+    var a, b, c float64
+    fmt.Scan(&a)
+    fmt.Scan(&b)
+    fmt.Scan(&c)
+
+    p := (a+b+c)/2
+    A := math.Sqrt(p*(p-a)*(p-b)*(p-c))
+
+    fmt.Printf("%.2f\n", A)
+}
