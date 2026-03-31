bce8d3f4912a23c4e19273b91ef38c223ffab188
JGabriel322 <joaogab322@gmail.com>
2026-03-31 12:32:47 +0000

    atv

--- ed/apagando/go/draft.go
+++ ed/apagando/go/draft.go
@@ -3,17 +3,34 @@ import "fmt"
 func main() {
 	var n int
 	fmt.Scan(&n)
-	identN := make([]int, n)
-	for i = 0; i < n; i++ {
-		fmt.Scan(&identN[i])
+
+	fila := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&fila[i])
 	}
 
 	var m int
 	fmt.Scan(&m)
-	identM := make([]int, n)
-	fot i = 0; i < n; i++ {
-		fmt.Scan(&identM[i])
+
+	saiu := make(map[int]bool)
+
+	for i := 0; i < m; i++ {
+		var x int
+		fmt.Scan(&x)
+		saiu[x] = true
 	}
 
+	result := make([]int, 0)
+	for i := 0; i < n; i++ {
+		if !saiu[fila[i]] {
+			result = append(result, fila[i])
+		}
+	}
+
+	for i := 0; i < len(result); i++ {
+		fmt.Printf("%d ", result[i])
+	}
+
+	fmt.Println()
 	
 }
--- ed/princesa/go/draft.go
+++ ed/princesa/go/draft.go
@@ -1,8 +1,40 @@
 package main
 import "fmt"
 func main() {
-    va n, e int
-    fmt.Scan(&n, &e)
+    var n, e int
+    fmt.Scanln(&n, &e)
 
-    
+    pessoas := make([]int, n)
+    for i := 0; i < n; i++ {
+        pessoas[i] = i+1
+    }
+
+    tamanho := n
+    indice := e-1
+
+    for tamanho > 1 {
+        fmt.Print("[ ")
+        for i := 0; i < tamanho; i++ {
+            if i == indice {
+                fmt.Printf("%d> ", pessoas[i])
+            } else {
+                fmt.Printf("%d ", pessoas[i])
+            }
+        }
+        fmt.Println("]")
+
+        vitima := (indice+1)%tamanho
+
+        for i := vitima; i < tamanho-1; i++ {
+            pessoas[i] = pessoas[1+i]
+        }
+
+        tamanho--
+        indice = vitima
+        if indice >= tamanho {
+            indice = 0
+        }
+    }
+
+    fmt.Printf("[ %d> ]\n", pessoas[0])
 }
